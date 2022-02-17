36b2330895d2310709cd6d8cf1fa219a68f1926c
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2022-02-17 09:41:39 +0200

    third-task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const formListener = document.querySelector(".feedback-form");
+
+const userData = {};
+dataFromlocalStorage();
+formListener.addEventListener("input", throttle(formData, 500));
+formListener.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(userData);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem("feedback-form-state");
+}
+
+function formData(e) {
+  userData[e.target.name] = e.target.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+}
+
+function dataFromlocalStorage() {
+  const saveData = localStorage.getItem("feedback-form-state");
+
+  if (saveData) {
+    //      //  если да, сохранни поченные значения из локалстореджа
+    //     console.log(saveMessage)
+    const parseSave = JSON.parse(saveData);
+    const keys = Object.keys(parseSave);
+
+    for (const key of keys) {
+      formListener.elements[key].value = parseSave[key];
+      userData[key] = parseSave[key];
+    }
+  }
+}
