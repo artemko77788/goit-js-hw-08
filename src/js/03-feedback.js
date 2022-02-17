2677e2ba3582af252c68fe4ac47308e6a6b5daa3
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2022-02-17 10:49:47 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,8 @@ import throttle from "lodash.throttle";
 const formListener = document.querySelector(".feedback-form");
 
 const userData = {};
+
+const Storage_Key = "feedback-form-state";
 dataFromlocalStorage();
 formListener.addEventListener("input", throttle(formData, 500));
 formListener.addEventListener("submit", onFormSubmit);
@@ -13,22 +15,23 @@ function onFormSubmit(evt) {
   console.log(userData);
 
   evt.currentTarget.reset();
-  localStorage.removeItem("feedback-form-state");
+  localStorage.removeItem(Storage_Key);
 }
 
 function formData(e) {
   userData[e.target.name] = e.target.value;
-  localStorage.setItem("feedback-form-state", JSON.stringify(userData));
+  localStorage.setItem(Storage_Key, JSON.stringify(userData));
 }
 
 function dataFromlocalStorage() {
-  const saveData = localStorage.getItem("feedback-form-state");
+  const saveData = localStorage.getItem(Storage_Key);
 
   if (saveData) {
     const parseSave = JSON.parse(saveData);
     const keys = Object.keys(parseSave);
 
     for (const key of keys) {
+      console.dir(formListener);
       formListener.elements[key].value = parseSave[key];
       userData[key] = parseSave[key];
     }
